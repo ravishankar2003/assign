61feeff6bfb345120ed3af563db20a6132eda1b3
ravishankar2003 <nidumukkalaravishankar@gmail.com>
2024-11-16 22:50:00 +0530

    update all

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/assign/',
+  build: {
+    rollupOptions: {
+      external: ['module-name'] // Replace 'module-name' with the problematic module
+    }
+  }
 })
